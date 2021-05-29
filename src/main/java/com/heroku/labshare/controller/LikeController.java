be2ba045587e8bd7210b0e30e619ac205657fdee
bboiko <79659123+bboiko-CLGX@users.noreply.github.com>
2021-05-29 19:14:59 +0300

    remove redundant- imports

--- src/main/java/com/heroku/labshare/controller/LikeController.java
+++ src/main/java/com/heroku/labshare/controller/LikeController.java
@@ -1,7 +1,5 @@
 package com.heroku.labshare.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
