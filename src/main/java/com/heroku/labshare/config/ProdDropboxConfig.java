08f04e03675ab1abdece666d94f50e105a8c9b8b
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2021-05-29 22:02:41 +0300

    hotfix: issue with env vars on prod

--- src/main/java/com/heroku/labshare/config/ProdDropboxConfig.java
+++ src/main/java/com/heroku/labshare/config/ProdDropboxConfig.java
@@ -6,8 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import static com.heroku.labshare.constant.EnvironmentVariableNames.DROPBOX_ACCESS_TOKEN;
-import static com.heroku.labshare.constant.EnvironmentVariableNames.DROPBOX_CLIENT_ID;
+import static com.heroku.labshare.constant.EnvironmentVariableNames.*;
 
 @Configuration
 @Profile("prod")
@@ -15,11 +14,13 @@ public class ProdDropboxConfig {
 
     @Bean
     DbxRequestConfig getDbxRequestConfig() {
-        return DbxRequestConfig.newBuilder(DROPBOX_CLIENT_ID).build();
+        String dropboxClientId = System.getenv(DROPBOX_CLIENT_ID);
+        return DbxRequestConfig.newBuilder(dropboxClientId).build();
     }
 
     @Bean
     DbxClientV2 getDbxClientV2(DbxRequestConfig config) {
-        return new DbxClientV2(config, DROPBOX_ACCESS_TOKEN);
+        String dropboxAccessToken = System.getenv(DROPBOX_ACCESS_TOKEN);
+        return new DbxClientV2(config, dropboxAccessToken);
     }
 }
